545be5cc46dc2e436fb7cdc36cc7d782a896f828
Leena Faisal <imalommer@gmail.com>
2023-11-28 03:28:31 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,129 +1,195 @@
-// sketch.js
-let planets;
+let player;
+let planets = [];
 let currentPlanet;
-let score = 0;
-let days = 1;
-let resources = 50;
-let explorationSpeed = 5;
-
-let pixelManipulated = false;
-let sunSize = 50;
-let sunColor = 255;
-
+let zoomLevel = 1;
+let infoPanel;
 let audio;
 let amplitude;
+let generatedText = "This is a placeholder for generated text.";
 
 function preload() {
-  audio = loadSound('offgun.mp3');
+  // Load your audio file
+  audio = loadSound("path/to/your/audio/file.mp3");
 }
 
 function setup() {
   createCanvas(800, 600);
-  planets = [
-    { name: "Mercury", quiz: { question: "What is the smallest planet in our solar system?", options: ["Venus", "Earth", "Mercury", "Mars"], correctAnswer: "Mercury" } },
-    { name: "Venus", quiz: { question: "Which planet is known as the 'Evening Star'?", options: ["Mercury", "Venus", "Mars", "Jupiter"], correctAnswer: "Venus" } },
-    { name: "Earth", quiz: { question: "What is the only planet known to support life?", options: ["Mars", "Earth", "Jupiter", "Saturn"], correctAnswer: "Earth" } },
-    { name: "Mars", quiz: { question: "Which planet is often called the 'Red Planet'?", options: ["Mars", "Jupiter", "Venus", "Uranus"], correctAnswer: "Mars" } },
-    { name: "Jupiter", quiz: { question: "Which planet is the largest in our solar system?", options: ["Saturn", "Mars", "Earth", "Jupiter"], correctAnswer: "Jupiter" } },
-    { name: "Saturn", quiz: { question: "Which planet is known for its prominent ring system?", options: ["Saturn", "Neptune", "Uranus", "Pluto"], correctAnswer: "Saturn" } },
-    // Add more planets with quiz questions
-  ];
-  currentPlanet = getRandomPlanet();
+
+  // Create player spaceship
+  player = new Spaceship(width / 2, height / 2);
+
+  // Create planets
+  planets.push(new Planet(200, 200, "Earth", "The third planet from the Sun.", "earth.jpg"));
+  planets.push(new Planet(400, 400, "Mars", "The fourth planet from the Sun.", "mars.jpg"));
+  planets.push(new Planet(600, 600, "Jupiter", "The largest planet in our solar system.", "jupiter.jpg"));
+
+  // Set the starting planet
+  currentPlanet = planets[0];
+
+  // Create info panel
+  infoPanel = new InfoPanel();
+
+  // Set up audio analysis
   amplitude = new p5.Amplitude();
+
+  // Set up text generation
+  generateText();
 }
 
 function draw() {
   background(0);
 
-  frameRate(explorationSpeed);
+  // Update and display player spaceship
+  player.update();
+  player.display();
 
-  if (pixelManipulated) {
-    manipulatePixels();
+  // Display and check interactions with planets
+  for (let planet of planets) {
+    planet.display();
+    if (planet.contains(player.x, player.y)) {
+      infoPanel.display(planet);
+      currentPlanet = planet;
+    }
   }
 
-  simulateSun();
+  // Display zoom level
+  textSize(16);
+  fill(255);
+  text("Zoom Level: " + zoomLevel.toFixed(2), 20, 20);
 
-  visualizeAudio();
+  // Display interactive audio visualization
+  displayAudioVisualization();
 
-  displayPlanet();
-  displayStats();
-  handleHover();
+  // Display generated text
+  displayGeneratedText();
+}
+
+function mouseWheel(event) {
+  // Zoom in/out when scrolling
+  zoomLevel += event.delta * 0.01;
+  zoomLevel = constrain(zoomLevel, 0.5, 2);
+}
+
+function keyPressed() {
+  // Control spaceship movement with arrow keys
+  if (keyCode === UP_ARROW) {
+    player.boost();
+  } else if (keyCode === RIGHT_ARROW) {
+    player.rotate(0.1);
+  } else if (keyCode === LEFT_ARROW) {
+    player.rotate(-0.1);
+  }
 }
 
 function mousePressed() {
-  if (mouseX >= 20 && mouseX <= 120 && mouseY >= 250 && mouseY <= 290) {
-    initiateQuiz();
-    pixelManipulated = true;
+  // Toggle play/pause for the audio
+  if (audio.isPlaying()) {
+    audio.pause();
+  } else {
     audio.play();
   }
 }
 
-function initiateQuiz() {
-  if (currentPlanet) {
-    let answer = window.prompt(currentPlanet.quiz.question + "\nOptions: " + currentPlanet.quiz.options.join(", "));
+// Define the Spaceship class
+class Spaceship {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.angle = 0;
+    this.speed = 0;
+  }
 
-    if (answer && answer.toLowerCase() === currentPlanet.quiz.correctAnswer.toLowerCase()) {
-      score += 10;
-      resources += 20;
-      currentPlanet = getRandomPlanet();
-    } else {
-      resources -= 10;
-    }
+  update() {
+    // Move the spaceship based on speed and angle
+    this.x += this.speed * cos(this.angle);
+    this.y += this.speed * sin(this.angle);
 
-    days++;
+    // Apply friction to slow down the spaceship
+    this.speed *= 0.98;
+
+    // Wrap around the screen
+    this.x = (this.x + width) % width;
+    this.y = (this.y + height) % height;
+  }
+
+  display() {
+    // Draw the spaceship
+    fill(255);
+    stroke(255);
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    triangle(0, -12, -8, 12, 8, 12);
+    pop();
+  }
+
+  boost() {
+    // Boost the speed of the spaceship
+    this.speed += 0.5;
+  }
+
+  rotate(angle) {
+    // Rotate the spaceship
+    this.angle += angle;
   }
 }
 
-function manipulatePixels() {
-  loadPixels();
-  for (let i = 0; i < pixels.length; i += 4) {
-    pixels[i] = 255 - pixels[i]; // Red
-    pixels[i + 1] = 255 - pixels[i + 1]; // Green
-    pixels[i + 2] = 255 - pixels[i + 2]; // Blue
+// Define the Planet class
+class Planet {
+  constructor(x, y, name, description, imageFileName) {
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.description = description;
+    this.image = loadImage(imageFileName);
   }
-  updatePixels();
+
+  display() {
+    // Draw the planet
+    imageMode(CENTER);
+    image(this.image, this.x, this.y, 50, 50);
+  }
+
+  contains(px, py) {
+    // Check if a point is inside the planet
+    let d = dist(px, py, this.x, this.y);
+    return d < 25;
+  }
 }
 
-function simulateSun() {
-  fill(sunColor, sunColor, 0);
-  ellipse(width - 100, 100, sunSize, sunSize);
-  sunSize += 0.5;
-  sunColor -= 0.2;
+// Define the InfoPanel class
+class InfoPanel {
+  display(planet) {
+    // Display information about the planet in an info panel
+    fill(255);
+    rect(200, 20, 400, 100);
+    textSize(16);
+    fill(0);
+    text(planet.name, 220, 40);
+    textSize(12);
+    text(planet.description, 220, 60);
+  }
 }
 
-function visualizeAudio() {
+// Define functions for audio visualization
+function displayAudioVisualization() {
+  // Display audio visualization code here
   let level = amplitude.getLevel();
-  let circleSize = map(level, 0, 1, 10, 200);
-  fill(0, 0, 255);
-  ellipse(width / 2, height / 2, circleSize, circleSize);
+  let size = map(level, 0, 1, 10, 200);
+  fill(255, 0, 0);
+  ellipse(width - 50, height - 50, size, size);
 }
 
-function getRandomPlanet() {
-  return random(planets);
+// Define functions for text generation
+function generateText() {
+  // Implement your text generation code here
+  // This is a placeholder. Replace with actual text generation logic.
+  generatedText = "This is a placeholder for generated text.";
 }
 
-function displayPlanet() {
-  textSize(24);
+function displayGeneratedText() {
+  // Display generated text code here
   fill(255);
-  textAlign(CENTER, TOP);
-  text(currentPlanet.name, width / 2, 10);
-  textSize(16);
-  textAlign(CENTER, CENTER);
-  text(currentPlanet.quiz.question, width / 2, height / 2);
-}
-
-function displayStats() {
-  textSize(18);
-  fill(255);
-  text("Score: " + score, 20, 20);
-  text("Days Spent Exploring: " + days, 20, 50);
-  text("Current Resources: " + resources, 20, 80);
-}
-
-function handleHover() {
-  if (mouseX >= 20 && mouseX <= 120 && mouseY >= 250 && mouseY <= 290) {
-    document.getElementById("controls").style.backgroundColor = "#8cf58c";
-  } else {
-    document.getElementById("controls").style.backgroundColor = "#f0f0f0";
-  }
+  textSize(14);
+  text(generatedText, 20, height - 50, width - 40, 100);
 }
